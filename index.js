a4b2557f9cb2c15891fea39d66e83700ff9d3a21
Sai Santosh Pal <78339151+Sai-Santosh-Pal@users.noreply.github.com>
2023-02-25 20:01:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,57 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const Stripe = require('stripe');
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const PORT = 8080;
 
+app.use("/stripe", express.raw({ type: "*/*" }));
 app.use(express.json());
 app.use(cors());
 
-app.post('/pay', async (req, res) => {
-    try {
-        const {name} = req.body;
-        if (!name) return res.status(400).json({ message: 'Please Enter A Name' })
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: Math.round(9 * 100),
-            currency: 'INR',
-            payment_method_types: ["card", "wallet"],
-            metadata: {name}
-        })
-        const clientSecret = paymentIntent.client_secret;
-        res.json({ message: "Payment Intiated", clientSecret})
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Internal Server Error' })
-    }
-})
+app.post("/pay", async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) return res.status(400).json({ message: "Please enter a name" });
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(25 * 100),
+      currency: "INR",
+      payment_method_types: ["card"],
+      metadata: { name },
+    });
+    const clientSecret = paymentIntent.client_secret;
+    res.json({ message: "Payment initiated", clientSecret });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
-app.listen(PORT, () => console.log('Server Working'))
+app.post("/stripe", async (req, res) => {
+  const sig = req.headers["stripe-signature"];
+  let event;
+  try {
+    event = await stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: err.message });
+  }
+
+  // Event when a payment is initiated
+  if (event.type === "payment_intent.created") {
+    console.log(`${event.data.object.metadata.name} initated payment!`);
+  }
+  // Event when a payment is succeeded
+  if (event.type === "payment_intent.succeeded") {
+    console.log(`${event.data.object.metadata.name} succeeded payment!`);
+    // fulfilment
+  }
+  res.json({ ok: true });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
